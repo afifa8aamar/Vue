9689c3de5ca34a4c61beef0cc9c85d6bdcb0a838
afifa8aamar <afifa.aamar@gmail.com>
2021-02-03 10:30:37 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Register/>
+</template>
+
+<script>
+
+import Register from './components/Register/Register'
+
+export default {
+  name: 'App',
+  components: {
+    Register,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
